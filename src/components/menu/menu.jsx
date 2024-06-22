027bbe5b38c0a7de0a5d73416626d9e74156562e
Hammad <142727701+Just-Hammad@users.noreply.github.com>
2024-06-22 08:30:29 +0500

    Update menu.jsx

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -4,7 +4,7 @@ import { useSound } from "../useSound";
 import open from "../../../public/audio/open.mp3";
 import beep from "../../../public/audio/beep.mp3";
 
-import gameoflife from "../../../public/svg/game-of-life.svg";
+import gameoflife from "../../../public/svg/gameoflife.svg";
 import bucket from "../../../public/svg/bucket.svg";
 import maze from "../../../public/svg/maze.svg";
 import chess from "../../../public/svg/chess.svg";
